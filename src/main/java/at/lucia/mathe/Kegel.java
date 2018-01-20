029ab5376212bd5901b060acf19d09b398364343
Lucia <lucia.avila.ceron@gmail.com>
2018-01-20 19:39:42 +0100

    none aufgabe 1

--- src/main/java/at/lucia/mathe/Kegel.java
+++ src/main/java/at/lucia/mathe/Kegel.java
@@ -3,17 +3,8 @@ package at.lucia.mathe;
 public class Kegel {
 
     public static double berechneOberflaeche(double durchmesser, double hoehe) {
-
         double radius = durchmesser / 2;
         double a2 = radius * radius;
-        double b2 = hoehe * hoehe;
-        double a2PlusB2 = a2 + b2;
-        double g = Math.sqrt(a2PlusB2);
-
-        double mantel = Math.PI * radius * g;
-        double grundflaeche = a2 * Math.PI;
-        double alles = mantel + grundflaeche;
-
-        return alles;
+        return Math.PI * radius * Math.sqrt(a2 + hoehe * hoehe) + a2 * Math.PI;
     }
 }
--- src/test/java/at/lucia/mathe/KegelTest.java
+++ src/test/java/at/lucia/mathe/KegelTest.java
@@ -8,18 +8,9 @@ public class KegelTest {
 
     @Test
     public void whenDurchMesserAndHoeheThenOberflaeche(){
-
         //Everything is in meters
-
-        double durchmesser = 6d;
-        double hoehe = 10d;
-
-        double oberflaeche = Kegel.berechneOberflaeche(durchmesser, hoehe);
-
+        double oberflaeche = Kegel.berechneOberflaeche(6d, 10d);
         assertThat(oberflaeche).isBetween(126.6d,126.7d);
-
-
-
     }
 
 }
