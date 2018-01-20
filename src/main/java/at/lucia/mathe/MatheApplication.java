751d910a8cbe313cea5b2306e2202c5d384b138f
Lucia <lucia.avila.ceron@gmail.com>
2018-01-20 18:58:06 +0100

    Mi proyecto

---
+++ src/main/java/at/lucia/mathe/MatheApplication.java
@@ -0,0 +1,12 @@
+package at.lucia.mathe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatheApplication.class, args);
+	}
+}
